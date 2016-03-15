4d6ac588a4f880d7321700db39e3413796da1055
Nicolas Garneau <ngarneau@ngarneau.com>
2016-03-15 09:49:02 -0400

    Initial Spark pipeline

---
+++ README.md
@@ -0,0 +1,7 @@
+# Semantic analysis on Spark
+
+To run the Spark application, assuming Spark binaries are in your path:
+
+```
+$ spark-submit --packages com.databricks:spark-csv_2.11:1.4.0 src/application.py
+```
---
+++ src/application.py
@@ -0,0 +1,33 @@
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+from pyspark.sql.types import *
+from pyspark.ml import Pipeline
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.feature import HashingTF, Tokenizer
+from pyspark.mllib.evaluation import BinaryClassificationMetrics
+
+
+sc = SparkContext("local", "Pipeline")
+sqlContext = SQLContext(sc)
+df = sqlContext.read.format('com.databricks.spark.csv').options(delimiter="\t", header='true', escape="\\").load('data/labeledTrainData.tsv')
+df = df.withColumn("label", df["label"].cast(DoubleType()))
+training, test = df.randomSplit([0.6, 0.4], seed=11)
+
+tokenizer = Tokenizer(inputCol="review", outputCol="words")
+hashingTF = HashingTF(inputCol=tokenizer.getOutputCol(), outputCol="features")
+lr = LogisticRegression(maxIter=10, regParam=0.01)
+pipeline = Pipeline(stages=[tokenizer, hashingTF, lr])
+
+model = pipeline.fit(training)
+
+prediction = model.transform(test)
+
+predictionAndLabels = prediction.map(lambda s: (s.prediction, s.label))
+
+metrics = BinaryClassificationMetrics(predictionAndLabels)
+
+# Area under precision-recall curve
+print("Area under PR = %s" % metrics.areaUnderPR)
+
+# Area under ROC curve
+print("Area under ROC = %s" % metrics.areaUnderROC)
